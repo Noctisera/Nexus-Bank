ce7b4d9a7c23059b34f5e8637338f86178bc57c1
Noctisera <pacuraru.macedoniu@gmail.com>
2024-02-29 10:43:53 +0200

    Addded packages and classes

---
+++ Main Project/src/Commands/Login.java
@@ -0,0 +1,7 @@
+package Commands;
+
+public class Login implements Command {
+    public void execute() {
+        System.out.println("Login command executed");
+    }
+}
---
+++ Main Project/src/Commands/Register.java
@@ -0,0 +1,13 @@
+package Commands;
+
+import Users.User;
+
+public class Register implements Command {
+    public void execute() {
+
+    }
+
+    private boolean UsernameExists(String username) {
+        return false;
+    }
+}
